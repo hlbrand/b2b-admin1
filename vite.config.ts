4a0150d6f000b09393af2ed6ec8ed8938f10e429
hlbrand <hlbrand29951@gmail.com>
2022-12-25 18:45:08 +0700

    add server railway

--- vite.config.ts
+++ vite.config.ts
@@ -9,31 +9,31 @@ import react from "@vitejs/plugin-react"
 dns.setDefaultResultOrder("verbatim")
 
 export default defineConfig({
-  plugins: [react()],
-  // Backwards-compat with Gatsby.
-  publicDir: "static",
-  build: {
-    outDir: "public",
-  },
-  resolve: {
-    alias: {
-      gatsby: path.resolve(__dirname, "src/compat/gatsby-compat.tsx"),
-      "@reach/router": path.resolve(
-        __dirname,
-        "src/compat/reach-router-compat.tsx"
-      ),
-    },
-  },
-  define: {
-    __MEDUSA_BACKEND_URL__: JSON.stringify(
-      env.MEDUSA_BACKEND_URL ||
-        // Backwards-compat with Gatsby.
-        env.GATSBY_MEDUSA_BACKEND_URL ||
-        env.GATSBY_STORE_URL ||
-        ""
-    ),
-  },
-  optimizeDeps: {
-    exclude: ["typeorm", "medusa-interfaces"],
-  },
+	plugins: [react()],
+	// Backwards-compat with Gatsby.
+	publicDir: "static",
+	build: {
+		outDir: "public",
+	},
+	resolve: {
+		alias: {
+			gatsby: path.resolve(__dirname, "src/compat/gatsby-compat.tsx"),
+			"@reach/router": path.resolve(
+				__dirname,
+				"src/compat/reach-router-compat.tsx"
+			),
+		},
+	},
+	define: {
+		__MEDUSA_BACKEND_URL__: JSON.stringify(
+			env.MEDUSA_BACKEND_URL ||
+			// Backwards-compat with Gatsby.
+			env.GATSBY_MEDUSA_BACKEND_URL ||
+			env.GATSBY_STORE_URL ||
+			"http://localhost:9000"
+		),
+	},
+	optimizeDeps: {
+		exclude: ["typeorm", "medusa-interfaces"],
+	},
 })
