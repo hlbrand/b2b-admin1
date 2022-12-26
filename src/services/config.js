c1586ef7571de833ed47294c378e7342c5e18e41
hlbrand <116349848+hlbrand@users.noreply.github.com>
2022-12-26 09:51:22 +0700

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
+// const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
 // const medusaUrl = "http://localhost:9000"
 // const medusaUrl = __MEDUSA_BACKEND_URL__
-// const medusaUrl = "https://b2b-server1-production.up.railway.app"
+const medusaUrl = "https://b2b-server1-production.up.railway.app"
 
 export { medusaUrl }
